01478ab8fa0c0a7e024cc0f7952af5621134c336
Dino <dinooo_06@hotmail.com>
2016-10-26 12:42:04 +0200

    biblioteka popunjena

--- src/DBSQL/Biblioteka.sql
+++ src/DBSQL/Biblioteka.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 25, 2016 at 02:18 PM
--- Server version: 5.7.15-0ubuntu0.16.04.1
+-- Generation Time: Oct 26, 2016 at 12:39 PM
+-- Server version: 5.7.16-0ubuntu0.16.04.1
 -- PHP Version: 7.0.8-0ubuntu0.16.04.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
